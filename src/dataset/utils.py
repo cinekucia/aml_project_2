99136a93aeb1ec88e453c4953c38cbfd58008ba1
Simonexc <szymekdt@gmail.com>
2024-06-03 22:35:25 +0200

    Update generation

--- src/dataset/utils.py
+++ src/dataset/utils.py
@@ -25,3 +25,16 @@ def get_cached_data(artifacts_path: str) -> tuple[np.ndarray, np.ndarray]:
     x = np.load(os.path.join(folders[0], 'x_train.npy'))
     y = np.load(os.path.join(folders[0], 'y_train.npy'))
     return x, y
+
+
+def get_cached_data_with_test(artifacts_path: str) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
+    folders = sorted(glob(
+        os.path.join(artifacts_path, f'{config.dataset_artifact_name}*')
+    ))[::-1]
+    if len(folders) == 0:
+        raise FileNotFoundError('No cached data found.')
+
+    x = np.load(os.path.join(folders[0], 'x_train.npy'))
+    y = np.load(os.path.join(folders[0], 'y_train.npy'))
+    x_test = np.load(os.path.join(folders[0], 'x_test.npy'))
+    return x, y, x_test
